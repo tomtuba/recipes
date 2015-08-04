862fe82033bce663ecc7a42b827c2cfb7c2ad095
tmccomb613 <tmccomb613@192.168.0.20>
2015-08-03 23:20:28 -0400

    Initial Project creation

---
+++ src/main/java/com/mccomb/recipe/controller/RecipeController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mccomb.recipe.controller;
+
+/**
+ *
+ * @author tmccomb613
+ */
+public class RecipeController {
+    
+}
